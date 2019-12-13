7ab41fc3bc4c6e017a0d2d7bcb8ca69b10f03d75
stefan <252556310@qq.com>
2019-12-13 18:40:02 +0800

    修改ttf文件引入方式

--- common/mixin/importIcon.js
+++ common/mixin/importIcon.js
@@ -1,12 +1,12 @@
 export default {
 	onLoad: function() {
 		// #ifdef APP-PLUS
-		const domModule = weex.requireModule('dom')
-		domModule.addRule('fontFace', {  
-		    'fontFamily': "iconfont2",  
-			'src': "url('https://at.alicdn.com/t/font_1494388_6p8busou8bx.ttf')"
-			// 'src': "url('"+"file://" + plus.io.convertLocalFileSystemURL("_www/static/iconfont.ttf") +"')"
-		});
+		// const domModule = weex.requireModule('dom')
+		// domModule.addRule('fontFace', {  
+		//     'fontFamily': "iconfont2",  
+		// 	'src': "url('https://at.alicdn.com/t/font_1494388_6p8busou8bx.ttf')"
+		// 	// 'src': "url('"+"file://" + plus.io.convertLocalFileSystemURL("_www/static/iconfont.ttf") +"')"
+		// });
 		// #endif
 	}
 }
